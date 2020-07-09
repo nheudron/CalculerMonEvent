ceb43f9fa53f1f42419f7b986d22a5329e24ab78
nheudron <Secure_150>
2020-07-09 17:58:42 +0200

    home page

--- index.php
+++ index.php
@@ -2,16 +2,30 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Document sans titre</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+<title>Calculer mon Evenement</title>
 </head>
-<p>Hello world</p>
+	
 <?php include("db_connexion.php")?>
+	
 	<header>
-		<img src="images/Comment-est-calculé-le-rendement-des-livrets-réglementés copie.jpg" alt="budget evenement">
+		<h1>calculermonevenement.com</h1>
+		<h2>En moins de 5 minutes </h2>
+		<img src="images/Comment-est-calculé-le-rendement-des-livrets-réglementés copie.jpg" class="imghead" alt="budget evenement">
 	</header>
-	<main> 
-		
+	
+	<main>
+		<div class="home">
+			<h1>CALCULERMONEVENEMENT.COM vous permet de définir votre budget pour votre événement.</h1>
+			<a href="form.php"><button>calculer mon budget</button></a>
+		</div>
 	</main>
+	
+	<footer>
+		<div></div>
+		<div></div>
+		<div></div>
+	</footer>
 <body>
 </body>
 </html>
