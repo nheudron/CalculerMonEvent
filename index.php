a64654dfb8cf871bad19fbf61c49b177e6def58c
nheudron <Secure_150>
2020-07-09 16:54:03 +0200

    first

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
 <meta charset="UTF-8">
 <title>Document sans titre</title>
 </head>
-	<p>Hello world</p>
-	
-	<?php include("db_connexion.php")?>
-	
+<p>Hello world</p>
+<?php include("db_connexion.php")?>
+	<header>
+		<img src="images/Comment-est-calculé-le-rendement-des-livrets-réglementés copie.jpg" alt="budget evenement">
+	</header>
+	<main> 
+		
+	</main>
 <body>
 </body>
 </html>
