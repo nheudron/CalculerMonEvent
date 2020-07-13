f8c4bfb48e9d660260605ee3d094e6a8697e5245
nheudron <Secure_150>
2020-07-13 15:18:37 +0200

    if user already exists, just create a session. If not, create a user with the form entries : it works well

--- formProcess.php
+++ formProcess.php
@@ -1,19 +1,23 @@
 <?php include("db_connexion.php");
-
-	createUser();
-
+	if (isset($_POST["email"])) {
+		$result1 = $db->prepare('SELECT * FROM user WHERE email = ? AND company = ?');
+		$result1->execute(array($_POST["email"], $_POST["company"]));
+		if ($result1->rowCount() == 1) { //check if there is one entry in the database
+			createSession(); //create a session with this user
+		}else{
+			createUser();//create a user with the form entries
+		}
+		$result1->closeCursor();
+		header("Location: form2.php");
+	}else{
+		header("Location: form.php");
+	}
+	
 	function createUser(){
 		global $db;
-
-		if(isset($_POST["email"])){
-			$Requestok = 1;
-		}else{
-			header("Location: form.php");
-		}
-
-		if ($Requestok == 1) {
-			$result = $db->prepare('INSERT INTO user(name, surname, company, email, phone) VALUES (:name, :surname, :company, :email, :phone)');
-			$result->execute(array(
+		
+			$result2 = $db->prepare('INSERT INTO user(name, surname, company, email, phone) VALUES (:name, :surname, :company, :email, :phone)');
+			$result2->execute(array(
 				'name'=>htmlspecialchars($_POST["name"]),
 				'surname'=>htmlspecialchars($_POST["surname"]),
 				'company'=>htmlspecialchars($_POST["company"]),
@@ -22,20 +26,17 @@
 			));
 			echo 'Les données ont été ajoutées.';
 			createSession();
-			header("Location: form2.php");
-		}else{
-			header("Location: form.php");
-		}
+			$result2->closeCursor();
 	}
 
-	function createSession(){ //start session
+	function createSession(){
 		session_start();
-		$result = $db->prepare('SELECT * FROM user WHERE Email_address = ? AND company = ?');
-		$result->execute(array($_POST["email"], $_POST["company"]));
-		$data = $result->fetch();
+		global $db;
+		$result3 = $db->prepare('SELECT * FROM user WHERE email = ? AND company = ?');
+		$result3->execute(array($_POST["email"], $_POST["company"]));
+		$data = $result3->fetch();
 		$_SESSION["email"] = $_POST["email"];
 		$_SESSION["user_id"] = $data["id"];
-		$result->closeCursor();
-		
+		$result3->closeCursor();
 	}
 ?>
